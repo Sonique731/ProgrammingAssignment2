f27609aadc5b055c2b92be1c07e13b49c58bb55d
Sonique <rosinavisagie@gmail.com>
2021-03-31 14:15:41 +0200

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
-
+x is a square invertible matrix
+retrun = a list containing functions to set the matrix, get the matrix,
+set the inverse, get the inverse
+this list is used as the input to cachSolve()
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+creates a special "matrix" object that can cache its inverse
 }
 
 
